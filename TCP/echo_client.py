583c06ebc525c27eb9a07071f3dcf5dc70c1ae52
Bozhidar Sabahov <33690328+darkopoppin@users.noreply.github.com>
2019-02-03 18:42:17 +0000

    Public_works
    Ip changed, + loop

--- TCP/echo_client.py
+++ TCP/echo_client.py
@@ -1,11 +1,14 @@
 import socket
 
-HOST = '127.0.0.1' #the server's ip address
-PORT = 65123 #the port user by the server
+HOST = '64.43.36.172' #the server's ip address
+PORT = 80 #the port user by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST,PORT))
-	s.sendall(b'Hello World') #read encoding strings in python in the txt
-	data = s.recv(1024)
+	i = 1
+	while i < 40:
+		s.sendall(b'Old age should burn and rave at close of day;Rage, rage against the dying of the light.') #read encoding strings in python in the txt
+		data = s.recv(1024)
+		i += 1
 
 print('Received', repr(data)) #read str vs repr in the txt
