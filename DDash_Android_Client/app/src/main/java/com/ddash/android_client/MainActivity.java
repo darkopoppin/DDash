15e8ad219187caa70c6c868583343306ff60367d
teabolt <tomasbaltrunas0@gmail.com>
2019-02-13 22:43:07 +0000

    Show phone build

--- DDash_Android_Client/app/src/main/java/com/ddash/android_client/MainActivity.java
+++ DDash_Android_Client/app/src/main/java/com/ddash/android_client/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ddash.android_client;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView keyField = findViewById(R.id.textView);
+        keyField.setText("Device model: ");
+
+        String devModel = Build.MODEL;
+        TextView valueField = findViewById(R.id.textView1);
+        valueField.setText(devModel);
     }
 }
