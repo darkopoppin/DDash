aed9ba07df98074611fcaec88fbb6be863dd1ce7
Marco <33690740+Marcowic@users.noreply.github.com>
2019-05-27 23:27:26 +0100

    Updated context retrieval

--- networkInfo/Network.java
+++ networkInfo/Network.java
@@ -1,10 +1,11 @@
-package com.example.networkdataminer;
+package com.bien.networkdataminer;
 
 import android.app.Application;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.view.View;
 
+import static android.content.Context.WIFI_SERVICE;
 /*
 Class for retrieving the network information
  */
@@ -17,13 +18,17 @@ public class Network extends Application {
 
 
     public Network() {
+        /*
+        Retrieve context from the 'MyApplication' class and store it in a variable. (One of the ways to access context from           anywhere).
+        Must supply the full path to the class in the package.
+        */
+        Context context = com.bien.Myapplication.getAppContext();
+
+        //Create WifiManager object with Context.
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(WIFI_SERVICE);
 
 
-        //Create WifiManager object from with Context.
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-
-
-        //Create WifiInfo object which will supply most/all the network information we need.
+        //Create WifiInfo object from the WifiManager which will supply most/all the network information we need.
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
         ip = wifiInfo.getIpAddress();
