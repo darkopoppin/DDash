a3f0c1af9d1f5096b998fadbbd57e094c89b6595
Marco <33690740+Marcowic@users.noreply.github.com>
2019-06-01 18:07:42 +0100

    Minor fix

--- networkInfo/MainActivity.java
+++ networkInfo/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.networkdataminer;
+package com.bien.networkdataminer;
 
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,23 +19,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txtWifiInfo = (TextView) findViewById(R.id.idTxt);
         btnInfo = (Button) findViewById(R.id.idBtn);
-    }
+        }
 
     public void getWifiInformation(View view) {
         /*
-        Attempt at invoking the Network class which I created. **Broken/Does not work**
+        Invoke the Network class.
          */
         Network network = new Network();
+
+        //Get the fields required.
         int ip = network.getIp();
         String macAddress = network.getmacAddress();
         int speed = network.getSpeed();
         int networkId = network.getNetworkId();
         String ssid = network.getSsid();
+
+        //Simple representation of the data gathered.
         String summary = "IP ADDRESS " + ip +
                 "\n" + "MAC ADDRESS " + macAddress +
                 "\n" + "LINK SPEED " + speed +
                 "\n" + "NETWORK ID " + networkId +
-                "\n" + "SSID " + ssid;
+                "\n" + "SSID " + ssid +
+                "\n" + "BUILD:" + Build.BOARD +
+                "\n" + "BOOTLOADER:" + Build.BOOTLOADER +
+                "\n" + "DEVICE:" + Build.DEVICE +
+                "\n" + "DISPLAY:" + Build.DISPLAY +
+                "\n" + "PRODUCT:" + Build.PRODUCT +
+                "\n" + "FINGERPRINT:" + Build.FINGERPRINT +
+                "\n" + "HOST:" + Build.HOST +
+                "\n" + "TYPE:" + Build.TYPE +
+                "\n" + "USER:" + Build.USER +
+                "\n" + "VERSION: " + Build.VERSION.RELEASE +
+                "\n" + "SDK: " + Build.VERSION.SDK_INT;
+
+        //Write to the TextView object on display.
         txtWifiInfo.setText(summary);
 
     }
