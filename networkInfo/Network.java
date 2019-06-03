9dd6b3e9bd6c21179f8ce1691d256834e62fc1f5
Marco <33690740+Marcowic@users.noreply.github.com>
2019-06-03 18:21:15 +0100

    added getAllWifiDetails(), convertIPtoString()
    Returns a list of all network related data.
    Added method to convert an IP address in numeric form into String.

--- networkInfo/Network.java
+++ networkInfo/Network.java
@@ -1,63 +1,98 @@
 package com.bien.networkdataminer;
-
-import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import java.util.ArrayList;
 
-
-import static android.content.Context.WIFI_SERVICE;
 
 /*
 Class for retrieving the network information
- */
+*/
 
 public class Network {
     // Initialising variables
-    private int ip, speed, networkId;
-    private String macAddress, ssid;
+    private int speed, networkId;
+    private String ip, macAddress, ssid;
 
 
-
-    public Network() {
-         /*
-        Retrieve context from the 'MyApplication' class and store it in a variable. (One of the ways to access context from anywhere).
-        Must supply the full path to the class in the package.
-        */
-        Context context = com.bien.MyApplication.getAppContext();
+    /*
+    Context is passed as a parameter to the init of the Network class
+    */
+    public Network(Object context) {
 
         //Create WifiManager object with Context.
-        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiManager wifiManager = (WifiManager) context;
 
 
         //Create WifiInfo object which will supply most/all the network information we need.
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
-        ip = wifiInfo.getIpAddress();
+        ip = convertIPtoString(wifiInfo);
+
+
         speed = wifiInfo.getLinkSpeed();
+
+
         networkId = wifiInfo.getNetworkId();
+
+
         macAddress = wifiInfo.getMacAddress();
+
+
         ssid = wifiInfo.getSSID();
 
 
     }
+    /*
+    Converts the IP from (int) numerical value to IPv4 format.
+    */
+    public String convertIPtoString(WifiInfo wifiInfo){
+        int ip;
+        ip = wifiInfo.getIpAddress();
+        String result;
+        result = String.format("%d.%d.%d.%d", (ip & 0xff), (ip >> 8 & 0xff), (ip >> 16 & 0xff),(ip >> 24 & 0xff));
 
-    public int getIp() {
-        return ip;
+        return result;
     }
+    /*
+    Getter for IP
+    */
+    public String getIp() {return ip;}
 
-    public int getNetworkId() {
-        return networkId;
-    }
+    /*
+    Getter for Network ID
+    */
+    public int getNetworkId() {return networkId;}
 
-    public int getSpeed() {
-        return speed;
-    }
 
-    public String getSsid() {
-        return ssid;
-    }
+    /*
+    Getter for Link Speed
+    */
+    public int getSpeed() {return speed;}
 
-    public String getmacAddress() {
-        return macAddress;
+
+    /*
+    Getter for Network SSID
+    */
+    public String getSsid() {return ssid;}
+
+
+    /*
+    Getter for Mac Address
+    */
+    public String getmacAddress() {return macAddress;}
+
+
+    /*
+    Returns all the network information in a List in the order: [ip address, Download speed (Mbps), Network ID, Mac Address, Network SSID]
+    */
+    public ArrayList getAllWifiDetails(){
+        ArrayList networkInfo = new ArrayList();
+        networkInfo.add(ip);
+        networkInfo.add(Integer.toString(speed));
+        networkInfo.add(Integer.toString(networkId));
+        networkInfo.add(macAddress);
+        networkInfo.add(ssid);
+
+        return networkInfo;
     }
 }
