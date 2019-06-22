ea10537fa0df500da48f4973c30a69785bd3537d
Bozhidar Sabahov <calidar9806@gmail.com>
2019-06-22 05:34:55 +0100

    Fixed repeating lines

--- DDash_Android_Client/app/src/main/java/com/ddash/MyApplication.java
+++ DDash_Android_Client/app/src/main/java/com/ddash/MyApplication.java
@@ -3,33 +3,20 @@ package com.ddash;
 import android.app.Application;
 import android.content.Context;
 
-
-        private static Context applicationContext;
-        private static Context context;
-
-        public void onCreate(){
-            super.onCreate();
-            applicationContext = getApplicationContext();
-            context = getAppContext();
-        }
-
-        public static Context getAppContext(){
-            return applicationContext;
-        }
-
-        public static Context getContext() { return  context; }
-
 public class MyApplication extends Application {
 
-    public static Context context;
+    private static Context applicationContext;
+    private static Context context;
 
     public void onCreate(){
         super.onCreate();
-        MyApplication.context = getApplicationContext();
-
+        applicationContext = getApplicationContext();
+        context = getAppContext();
     }
 
     public static Context getAppContext(){
-        return MyApplication.context;
+        return applicationContext;
     }
+
+    public static Context getContext() { return  context; }
 }
