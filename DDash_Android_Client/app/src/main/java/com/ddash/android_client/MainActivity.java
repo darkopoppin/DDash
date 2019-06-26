fd156e9ce6763554e9e2e57961b1707941d4b791
Maruko <mjorvina2016@gmail.com>
2019-06-26 18:50:08 +0100

    external storage fix, added system card

--- DDash_Android_Client/app/src/main/java/com/ddash/android_client/MainActivity.java
+++ DDash_Android_Client/app/src/main/java/com/ddash/android_client/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             if (extStorage == null){
                 externalText.setText("No sd card.");
             } else {
-                internalText.setText(String.format("%.1f of %.1f free", Utils.convertBytes(extStorage.get(1)), Utils.convertBytes(extStorage.get(0))));
+                externalText.setText(String.format("%.1f of %.1f free", Utils.convertBytes(extStorage.get(1)), Utils.convertBytes(extStorage.get(0))));
             }
 
 
@@ -223,7 +223,11 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 //        }
         return data;
     }
+    public void getSystemData(){
+        Map<String, Object> systemData = SystemData.getSystemData(getApplicationContext());
+        Log.d(TAG, "getSystemData: The following are the system data" + systemData.toString());
 
+    }
     public void getDownloadSpeed(View view){
         Log.d(TAG,"getDownloadSpeed : Commencing Internet download speed ");
         TextView test = findViewById(R.id.main_text_netspeed);
